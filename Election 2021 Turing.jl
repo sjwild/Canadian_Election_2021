f7c2af8ddb59e4766a868fefdd68f42201589f6d
Stephen Wild <stephen.james.wild@gmail.com>
2021-07-31 20:38:23 -0400

    julia model

---
+++ Election 2021 Turing.jl
@@ -0,0 +1,217 @@
+using Plots, StatsPlots
+using DataFrames, CSV
+using Turing, ReverseDiff, Memoization
+using RDatasets
+using StatsFuns
+using Dates
+using JLD
+using LinearAlgebra
+using Measures
+using Random 
+
+# Helper function
+function calc_moe(x, ss)
+    return  sqrt(x * (1-x) / ss)
+
+end
+
+# Dates
+dateformat = DateFormat("y-m-d")
+election_day_2015 = Date(2015, 10, 19)
+election_day_2019 = Date(2019, 10, 21)
+election_day_2021 = Date(2021, 09, 30)
+
+# Load pre-cleaned polls
+can_polls = CSV.read("can_poll2.csv", DataFrame; missingstring ="NA")
+can_polls = dropmissing(can_polls, [:BQ, :GPC])
+can_polls[:, :Other] = [1 - sum(can_polls[i,[:LPC, :CPC, :NDP, :BQ, :GPC]]) for i in 1:size(can_polls, 1)]
+can_polls = can_polls[can_polls.Other .> 0.0000, :]
+
+# Pollster id
+#pollster_dict = Dict(key => idx for (idx, key) in enumerate(unique(can_polls.Polling_firm)))
+#can_polls.pollster_id = [pollster_dict[i] for i in can_polls.Polling_firm]
+
+can_polls.poll_date = election_day_2015 .+ Dates.Day.(can_polls.NumDays) .- Dates.Day(1)
+
+
+# Prep data for model
+parties = ["LPC", "CPC", "NDP", "BQ", "GPC"]
+election_2019 = Dates.value(election_day_2019 - election_day_2015) + 1
+N_days = Dates.value(election_day_2021 - election_day_2015) + 1
+N_polls = size(can_polls, 1)
+N_pollsters = length(unique(can_polls.pollster_id))
+N_parties = length(parties)
+N_modes = length(unique(can_polls.mode_id))
+y_mat = Matrix(can_polls[:, parties])
+y_mat_moe = Matrix(calc_moe.(y_mat, can_polls.SampleSize))
+start_election = Vector([.395, .319, .197, 0.047, 0.034])
+end_election = Vector([.331, .343, 0.16, 0.076, 0.065]) 
+poll_date = convert.(Int64, can_polls.NumDays)
+poll_id = [1:size(can_polls, 1);]
+pollster_id = can_polls.pollster_id
+mode_id = can_polls.mode_id
+
+
+# Turing model for pooling polls
+@model function state_space_elections(
+    y::Matrix, 
+    y_moe::Matrix, 
+    start_election::Vector, 
+    end_election::Vector, 
+    poll_date::Vector{Int}, 
+    poll_id::Vector{Int}, 
+    N_days::Int, 
+    N_polls::Int,
+    N_modes::Int,
+    election_2019::Int,
+    N_pollsters::Int, 
+    N_parties::Int,
+    pollster_id::Vector{Int}, 
+    mode_id::Vector{Int},
+    ::Type{T} = Float64) where {T}
+
+
+    # empty containers
+    ξ = Matrix{T}(undef, (N_days, N_parties))
+    σ = Matrix{T}(undef, (N_polls, N_parties))
+    μ = Matrix{T}(undef, (N_polls, N_parties))
+    
+
+    # Omega and Rho for non-centered parameterization
+    z_ω ~ filldist(Normal(0, 1), N_parties, (N_days - 2))
+    ω ~ filldist(Normal(0, 0.05), N_parties)
+    ρ ~ LKJ(N_parties, 2.0)
+
+
+    # House effects
+    δ ~ filldist(Normal(0, 0.05), N_pollsters, N_parties)
+    
+
+    # sigmas for party and pollster-by-party effects    
+    σ_party ~ filldist(Exponential(1/20), N_parties)
+    σ_pollster ~ filldist(Exponential(1/20), N_pollsters, N_parties)
+    σ_mode ~ filldist(Exponential(1/20), N_modes, N_parties)
+
+
+    # Transform parameters
+    Ω = LinearAlgebra.diagm(ω) * ρ * LinearAlgebra.diagm(ω) * z_ω
+
+    ξ[1, :] = start_election
+    ξ[election_2019, :] = end_election    
+    
+    # for loop to fill in random walk priors
+    for t in 2:(election_2019 - 1)
+        for j in 1:N_parties
+            ξ[t,j] = ξ[t-1, j] + Ω[j, t-1]
+        end
+    end
+
+    for tt in (election_2019 + 1):(N_days)
+        for j in 1:N_parties
+            ξ[tt,j] = ξ[tt - 1, j] + Ω[j, tt - 2]
+        end
+    end   
+
+    # for loops to run model
+    for i in 1:N_polls
+        for j in 1:N_parties
+            σ[i, j] = sqrt(σ_party[j]^2 + σ_pollster[pollster_id[i], j]^2 + σ_mode[mode_id[i], j]^2 + y_moe[i, j]^2)
+            #σ[i, j] = σ_party[j] + σ_pollster[pollster_id[i], j] + σ_mode[mode_id[i], j] + y_moe[i, j] # error also occurs if I use this line
+            μ[i, j] = ξ[poll_date[i], j] + δ[pollster_id[i], j]
+            y[i, j] ~ Normal(μ[i, j], σ[i, j])
+        end
+    end 
+
+    for j in 1:N_parties
+        end_election[j] ~ Normal(ξ[election_2019 - 1, j], 0.001)
+    end
+
+    return ξ
+    #return ξ, σ, μ
+
+end
+
+mod_election = state_space_elections(y_mat,
+                                     y_mat_moe,
+                                     start_election, 
+                                     end_election,
+                                     poll_date,
+                                     poll_id,
+                                     N_days,
+                                     N_polls,
+                                     N_modes,
+                                     election_2019,
+                                     N_pollsters,
+                                     N_parties,
+                                     pollster_id,
+                                     mode_id)
+
+
+
+# Set iters and ids
+n_adapt = 1250
+n_iter = 750
+n_chains = 4
+
+
+#Random.seed!(4329)
+Random.seed!(53103)
+Turing.setadbackend(:reversediff)
+Turing.setrdcache(true)
+chns_election = sample(mod_election, NUTS(n_adapt, 0.8; max_depth = 12), MCMCThreads(), n_iter, n_chains)
+
+
+# Save chains
+save("turing_model_can_election.jld", "chns_election", chns_election)
+
+
+# Generate ξ
+ξ_gq = generated_quantities(mod_election, chns_election)
+
+rs = n_iter * n_chains
+ξ = Array{Float64}(undef, (rs, N_days, N_parties))
+
+for i in 1:rs
+    tmp = collect(ξ_gq[i])
+    for j in 1:N_days
+        for p in 1:N_parties
+        ξ[i, j, p] = tmp[j, p]
+        end
+    end
+end
+
+ξ_ll = Matrix{Float64}(undef, (N_days, N_parties))
+ξ_m = Matrix{Float64}(undef, (N_days, N_parties))
+ξ_uu = Matrix{Float64}(undef, (N_days, N_parties))
+
+for j in 1:N_days
+    for p in 1:N_parties
+        ξ_ll[j,p] = quantile(ξ[: ,j, p], 0.025)
+        ξ_m[j,p] = quantile(ξ[: ,j, p], 0.50)
+        ξ_uu[j,p] = quantile(ξ[: ,j, p], 0.975)
+    end
+end
+
+
+
+# Plot ξ and polls over time
+num_days = election_day_2015 .+ Dates.Day.(1:N_days) .- Dates.Day(1)
+colours = [:red, :blue, :orange, :cyan, :green]
+
+plt = plot(size = (750, 500), legend = :topright, fontfamily = :Courier, left_margin = 10mm, bottom_margin = 15mm, ylabel = "Vote intention (%)")
+ylims!(plt, (0.0, 0.6))
+for i in 1:length(colours)
+    scatter!(plt, (can_polls.poll_date), can_polls[:, parties[i]], label = parties[i], mc = colours[i])
+    plot!(plt, num_days, ξ_m[:,i], ribbon = (ξ_m[:,i] - ξ_ll[:,i], ξ_uu[:,i] - ξ_m[:,i]), 
+          label = nothing, fc = colours[i], lc = colours[i], lw = 2)
+end
+
+title!(plt, "Estimated vote intention of Canadian voters:\n2015 to 2021", title_align= :left, titlefontsize = 12)
+annotate!(plt, num_days[end], -0.08, StatsPlots.text("Source: Wikipedia. Analysis by sjwild.github.io\nUpdated July 30, 2021", :lower, :right, 8, :grey))
+yticks!(plt, [0.0, 0.1, 0.2, 0.3, 0.4, 0.5], 
+             ["0", "10", "20", "30", "40", "50"])
+
+savefig(plt, "can_vote_intention_2015_2021.png")
+
+
+
--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Canandian_Election_2021
 
-This is an attempt to model the upcoming 2021 (?) Canadian federal election using a state space model in Stan. The model assumes that there is an underlying latent voting intention for each of the five main parties (plus others) that is measured by a bunch of noisy polls.  
+This is an attempt to model the upcoming 2021 (?) Canadian federal election using a state space model.  The first model is produced using R and Stan, while the second is a similar version produced using Julia and Turing.jl. Both models deliver similar results.
 
-The model is based on the model suggested by Simon Jackman in his book, _Bayesian Analysis for the Social Sciences_, and updated by [Peter Ellis]( http://freerangestats.info/elections/oz-2019/index.html). My version differs from their versions in the following ways:  
+The model assumes that there is an underlying latent voting intention for each of the five main parties (plus others) that is measured by a bunch of noisy polls.  
 
-* I use the non-centered paramterization to improve sampling of the innovation sd 
+The model is based on the model suggested by Simon Jackman in his book, _Bayesian Analysis for the Social Sciences_, and updated by [Peter Ellis]( http://freerangestats.info/elections/oz-2019/index.html). My Stan and Turing version differ from their versions in the following ways:  
+
+* I use the non-centered paramterization to improve sampling of the innovation sd (Stan)
 * I anchor the election results, and use the polling error to predict voting intention for the period after the 2019 election 
 * In addition to house effects, I decompose the polling error into four sources:  
   * the margin of error based on simple random sampling 
@@ -12,8 +14,29 @@ The model is based on the model suggested by Simon Jackman in his book, _Bayesia
   * a mode-by-party error
   * pollster-by-party error
 
+The Julia model is run using Turing.jl It is coded using a sligthyl different way of creating a covariance matrix, so that I am able to use Distributions.jl and Turing.jl. I will (hopefully) recode the model when (if?) Distributions.jl gets a native cholesky decomposition of the LKJ distribution.
+
 The predictions for voting intention (that is, for the period after 2019-10-21, the date of 2019 Canadian election) assume that the polling errors are the same after the election as before it. That may or may not be true. I suspect that polling errors are correlated between periods.
 
+## Vote intention
+As of July 30, 2021, these are the estimated vote shares based on Canadian polls:
+
+|**Party**    | **Vote share**  | **95% bounds**     |
+|-------------|:---------------:|:------------------:|
+|**LPC**      | 36.3%           | (34.6%, 38.6%)     |
+|**CPC**      | 31.1%           | (29.2%, 33.0%)     |
+|**NDP**      | 17.3%           | (15.5%, 19.1%)     |
+|**BQ**       | 7.1%            | (6.3%, 7.9%)       |
+|**GPC**      | 3.6%            | (4.1%, 5.9%)       |
+
+
+
+![alt text](https://github.com/sjwild/Canandian_Election_2021/raw/main/can_vote_intention_2015_2021.png "Vote share of Canadian parties from 2015 to 2021.")
+
+
+
+# Future plans
+I plan to eventually produce a second stage of the model to estimate seat distributions. If time permits, I also plan to try produce a third model to estimate riding-by-riding outcomes.
 
 
 
@@ -22,17 +45,29 @@ The predictions for voting intention (that is, for the period after 2019-10-21, 
 ## Other state space models
 
 Bailey, J. (2021). britpol v0.1.0: User Guide and Data Codebook. Retrieved from https://doi.org/10.17605/OSF.IO/2M9GB.  
-Economist (2020.) Forecasting the US elections. Retrieved from https://projects.economist.com/us-2020-forecast/president.  
+
+Economist (2020.) Forecasting the US elections. Retrieved from https://projects.economist.com/us-2020-forecast/president. 
+
 Ellis, P. (2019). ozfedelect R package. Retrieved from https://github.com/ellisp/ozfedelect.   
+
 Savage, J.(2016). Trump for President? Aggregating National Polling Data. Retrieved from https://khakieconomics.github.io/2016/09/06/aggregating-polls-with-gaussian-Processes.html.  
+
 INWT Statistics GmbH (2021). Election forecast. Retrieved from https://github.com/INWTlab/lsTerm-election-forecast.  
 
 ## Articles
 Bélanger, É., & Godbout, J. F. (2010). Forecasting Canadian federal elections. _PS: Political Science & Politics_, _43_(4), 691-699.    
+
 Heidemanns, M., Gelman, A., & Morris, G. E. (2020). An updated dynamic Bayesian forecasting model for the US presidential election. _Harvard Data Science Review_, _2_(4).  
+
 Linzer, D. A. (2013). Dynamic Bayesian forecasting of presidential elections in the states. Journal of the American Statistical Association, 108(501), 124-134.   
+
 MacInnis, B., Krosnick, J. A., Ho, A. S., & Cho, M. J. (2018). The accuracy of measurements with probability and nonprobability survey samples: Replication and extension. _Public Opinion Quarterly_, _82_(4), 707-744.  
-Mongrain, P., Nadeau, R., & Jérôme, B. (2021). Playing the synthesizer with Canadian data: Adding polls to a structural forecasting model. _International Journal of Forecasting_, _37_(1), 289-301.    
+
+Mongrain, P., Nadeau, R., & Jérôme, B. (2021). Playing the synthesizer with Canadian data: Adding polls to a structural forecasting model. _International Journal of Forecasting_, _37_(1), 289-301.   
+
 Shirani-Mehr, H., Rothschild, D., Goel, S., & Gelman, A. (2018). Disentangling bias and variance in election polls. _Journal of the American Statistical Association_, _113_(522), 607-614.  
+
 Walther, D. (2015). Picking the winner (s): Forecasting elections in multiparty systems. _Electoral Studies_, _40_, 1-13.  
+
+
 
